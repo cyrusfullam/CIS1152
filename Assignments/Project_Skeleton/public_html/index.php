e47e0307dfdd6ff2ab9bf66de7ecc62140dce64d
unknown <cmf08270@CLK310-09.vtc.vsc.edu>
2015-01-13 19:43:43 -0500

    done

---
+++ Assignments/Project_Skeleton/public_html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>hello world</title>
+<link rel="stylesheet" href="./css/helloWorldStyle.css" type="text/css"/>
+</head>
+<body>
+<h1>doesnt this look good in purple</h1>
+<p>i thought this wonderfull shade of green was nice</p>
+<?php
+ echo "Hello World!";
+?>
+</body>
+</html>
